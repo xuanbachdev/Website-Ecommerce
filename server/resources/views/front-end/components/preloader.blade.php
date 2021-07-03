6bfde93ff30ff074e5b7b4ad15c4c542c0c883e1
Xuân Bách <58884736+xuanbachdev@users.noreply.github.com>
2021-07-03 16:38:45 +0700

    Update preloader.blade.php

--- server/resources/views/front-end/components/preloader.blade.php
+++ server/resources/views/front-end/components/preloader.blade.php
@@ -8,29 +8,32 @@
             <div class="animation-preloader">
                 <div class="icon"><img src="{{ asset('frontend/images/1.svg') }}" alt=""></div>
                 <div class="txt-loading">
-								<span data-text-preloader="X" class="letters-loading">
-									X
-								</span>
-                    <span data-text-preloader="U" class="letters-loading">
-									U
-								</span>
-                    <span data-text-preloader="Â" class="letters-loading">
-									Â
+								<span data-text-preloader="A" class="letters-loading">
+									A
 								</span>
                     <span data-text-preloader="N" class="letters-loading">
 									N
 								</span>
-                    <span data-text-preloader="B" class="letters-loading">
-									B
+                    <span data-text-preloader="O" class="letters-loading">
+									O
 								</span>
-                    <span data-text-preloader="Á" class="letters-loading">
-									Á
+                    <span data-text-preloader="N" class="letters-loading">
+									N
 								</span>
-                    <span data-text-preloader="C" class="letters-loading">
-									C
+                    <span data-text-preloader="Y" class="letters-loading">
+									Y
 								</span>
-                    <span data-text-preloader="H" class="letters-loading">
-									H
+                    <span data-text-preloader="M" class="letters-loading">
+									M
+								</span>
+                    <span data-text-preloader="O" class="letters-loading">
+									O
+								</span>
+                    <span data-text-preloader="U" class="letters-loading">
+									U
+								</span>
+					<span data-text-preloader="S" class="letters-loading">
+									S
 								</span>
                 </div>
             </div>
