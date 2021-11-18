4a0316c0394acd9bc8a43aff23c20fd991b6cf23
xuanbachdev <xuanbachwebdev@gmail.com>
2021-11-19 00:50:17 +0700

    website ecommerce

--- resources/views/back-end/partials/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- Main Footer -->
-<footer class="main-footer" style="display:flex; justify-content:center">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-
-    </div>
-    <!-- Default to the left -->
-    <strong >Copyright &copy; 2021 <a href="https://www.facebook.com/xuanbachpc">Coder</a></strong>
-</footer>
